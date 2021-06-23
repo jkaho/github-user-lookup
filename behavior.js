34848f32a5cf0d84a63bc0c65dbb2e77163ab2be
jkaho <72959522+jkaho@users.noreply.github.com>
2021-06-23 20:22:14 +1000

    Use fetch API to retrieve github api user data

--- behavior.js
+++ behavior.js
@@ -2,10 +2,21 @@
 
 const usernameForm = document.getElementById("username-form");
 const usernameInput = document.getElementById("username-input");
+const resultP = document.getElementById("result");
 
 function handleUsernameFormSubmit(e) {
   e.preventDefault();
   const username = usernameInput.value;
+  fetch(`https://api.github.com/users/${username}`, {
+    method: "GET",
+    headers: {
+      accept: "application/vnd.github.v3+json"
+    }
+  }).then(res => res.json())
+    .then(data => {
+      resultP.innerHTML = `${username} has <span class="bold-text">${data.public_repos}</span> repositories!`
+    })
+    .catch(err => console.log(err))
 }
 
 usernameForm.addEventListener("submit", handleUsernameFormSubmit);
