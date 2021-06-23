76d41471e7f3fdaf48d9ab66640b5508c7668160
jkaho <72959522+jkaho@users.noreply.github.com>
2021-06-23 19:04:25 +1000

    Create js DOM references, add submit event listener to username form

--- behavior.js
+++ behavior.js
@@ -1,1 +1,8 @@
-console.log('testing');
+// console.log('testing');
+
+const usernameForm = document.getElementById("username-form");
+const usernameInput = document.getElementById("username-input");
+
+function handleUsernameFormSubmit() {}
+
+usernameForm.addEventListener("submit", handleUsernameFormSubmit);
