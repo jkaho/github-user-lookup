336a4cf1fda4d14f802183361ae7c8f042591982
jkaho <72959522+jkaho@users.noreply.github.com>
2021-06-24 23:49:54 +1000

    Remove console log statement for API response

--- behavior.js
+++ behavior.js
@@ -14,7 +14,6 @@ function handleUsernameFormSubmit(e) {
     }
   }).then(res => res.json())
     .then(data => {
-      console.log(data)
       let str;
       if (data.message) {
         str = `"${username}" is not a valid GitHub user.`
